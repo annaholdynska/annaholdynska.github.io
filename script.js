a6329c26e37091da08933a1622fc4271b5740419
annaholdynska <anna.holdynska@grupabemo.pl>
2026-06-21 21:33:07 +0200

    Dodaje komentarze do kodu js

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
-// będziemy obsługiwać grida klocków umiejętności
+// ---------------------------------- zmienne ----------------------------------
+
+// szuka w html elementu z klasą .skills-grid i przypisuje do zmiennej, po to żeby móc reagować na ten element
 const skillsGrid = document.querySelector(".skills-grid");
 
-// przypisujemy do zmiennej description element o id "skill-description"
+// przypisujemy do zmiennej description element o id "skill-description", bo w tym miejscu będą wyświetlane opisy umiejętności
 const description = document.getElementById("skill-description");
 
-// pobiera wszystkie klocki
+// szuka wszystkich elementów z klasą .skills-item i przypisuje do zmiennej, po to żeby każdemu z nich dodać click itp.
 const skillItems = document.querySelectorAll(".skills-item");
 
-// przechowuje aktualnie kliknięty klocek
+// let = zmienna, której można zmieniać wartość (let selectedItem = null - to jest początkowa wartość zmiennej), const = zmienna, której nie można zmieniać
+
+// przechowuje aktualnie kliknięty klocek - jest po to, żeby pamiętać, co użytkownik wybrał
 let selectedItem = null;
+// tu trzeba pamiętać, który klocek był ostatnio najechany myszką, żeby po zjechaniu myszką z klocka, który nie jest wybrany, pokazać opis ostatniego najechanego klocka
 let lastHoveredItem = null;
 
-// przechodzi po wszystkich klockach
+// ---------------------------------- reakcja na najechanie myszką ----------------------------------
+
+// robimy pętle, która przechodzi po każdym klocku, item to jest jak "i" w pythonie
 skillItems.forEach((item) => {
-  // reakcja na najechanie myszką
+  // kiedy mysz najedzie na klocek
   item.addEventListener("mouseenter", () => {
-    // jeśli jakiś klocek jest już wybrany
+    // jeśli jakiś klocek jest wciśnięty to funkcja "mouseenter" ma się cała NIE wykonać
     if (selectedItem) {
-      return; // zakończ funkcję
+      return; // zakończ funkcję "mouseenter", nie idź dalej do linijek
     }
-    // pobiera opis z data-description
+    // tworzymy zmienną "text" tylko dla czytelności kodu, item.dataset.description pobiera opis wybranej umiejętności z htmla, żeby wiedzieć którą pokazać na ekranie
     const text = item.dataset.description;
 
-    // wstawia opis do elementu na górze
+    // text.Content to właściwość DOM, która pobiera i wstawia czysty tekst w elemencie HTML, wstawia na ekran zmienną "text" - umiejętność, która została wybrana krok wcześniej
     description.textContent = text;
+
+    // do zmiennej "lastHoveredItem" przypisuje ostatni klocek, na którym była najechana mysz
     lastHoveredItem = item;
   });
+
+  // tu skończyliśmy xd
 
   // reakcja na zjechanie myszką
   item.addEventListener("mouseleave", () => {
